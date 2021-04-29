b5964484ca1747d1a7078c3ac9114fb86a5a45da
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-04-29 22:04:00 +0530

    Add trending example.

---
+++ examples/trending.rs
@@ -0,0 +1,18 @@
+use piped::piped::PipedClient;
+use reqwest::ClientBuilder;
+
+#[tokio::main]
+async fn main() {
+    let httpclient = ClientBuilder::new()
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0")
+        .build()
+        .unwrap();
+
+    let instance = "https://pipedapi.kavin.rocks".to_string();
+
+    let client = PipedClient::new(httpclient, instance);
+
+    let channel = client.get_trending().await.unwrap();
+
+    println!("{:?}", channel);
+}
