9f2ccc35ee196400b06948dad708a040cdd796a4
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-04-29 20:56:18 +0530

    Restructure project and add examples.

---
+++ examples/channel.rs
@@ -0,0 +1,21 @@
+use piped::piped::PipedClient;
+use reqwest::ClientBuilder;
+
+#[tokio::main]
+async fn main() {
+    let httpclient = ClientBuilder::new()
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0")
+        .build()
+        .unwrap();
+
+    let instance = "https://pipedapi.kavin.rocks".to_string();
+
+    let client = PipedClient::new(httpclient, instance);
+
+    let channel = client
+        .get_channel_from_id("UCXuqSBlHAE6Xw-yeJA0Tunw".to_string())
+        .await
+        .unwrap();
+
+    println!("{:?}", channel);
+}
