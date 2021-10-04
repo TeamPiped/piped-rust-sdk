431bf31fcc361d0122941ea1a58079c7697919ce
Julian Schmidhuber <github@schmiddi.anonaddy.com>
2021-10-04 17:37:00 +0200

    Make functions take references, moved user agent into lib

--- examples/comments.rs
+++ examples/comments.rs
@@ -1,16 +1,13 @@
 use piped::PipedClient;
-use reqwest::ClientBuilder;
+use reqwest::Client;
+
+const INSTANCE: &'static str = "https://pipedapi.kavin.rocks";
 
 #[tokio::main]
 async fn main() {
-    let httpclient = ClientBuilder::new()
-        .user_agent("Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0")
-        .build()
-        .unwrap();
+    let httpclient = Client::new();
 
-    let instance = "https://pipedapi.kavin.rocks".to_string();
-
-    let client = PipedClient::new(httpclient, instance);
+    let client = PipedClient::new(&httpclient, INSTANCE);
 
     let comments = client
         .comments_from_id("__hYx6ZzFbQ".to_string())
--- piped/src/client.rs
+++ piped/src/client.rs
@@ -7,32 +7,37 @@ pub struct PipedClient {
     pub instance: String,
 }
 
+const USER_AGENT: &'static str = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0";
+
 impl PipedClient {
-    pub fn new(httpclient: Client, instance: String) -> PipedClient {
+    pub fn new<S: AsRef<str>>(httpclient: &Client, instance: S) -> PipedClient {
         PipedClient {
-            httpclient,
-            instance,
+            httpclient: httpclient.clone(),
+            instance: instance.as_ref().to_string(),
         }
     }
 
-    pub async fn trending(
+    pub async fn trending<S: AsRef<str>>(
         &self,
-        region: String,
+        region: S,
     ) -> Result<Vec<RelatedStream>, Box<dyn std::error::Error>> {
         let mut url = Url::parse(format!("{}/trending", &self.instance).as_str())?;
-        url.query_pairs_mut().append_pair("region", region.as_str());
+        url.query_pairs_mut().append_pair("region", region.as_ref());
 
-        let resp = &self.httpclient.get(url).send().await?.text().await?;
+        let resp = &self.httpclient.get(url)
+            .header("User-Agent", USER_AGENT)
+            .send().await?.text().await?;
 
-        let streams: Vec<RelatedStream> = serde_json::from_str(resp.as_str())?;
+        let streams: Vec<RelatedStream> = serde_json::from_str(resp.as_ref())?;
 
         Ok(streams)
     }
 
-    pub async fn channel_from_id(&self, id: String) -> Result<Channel, Box<dyn std::error::Error>> {
+    pub async fn channel_from_id<S: AsRef<str>>(&self, id: S) -> Result<Channel, Box<dyn std::error::Error>> {
         let resp = &self
             .httpclient
-            .get(format!("{}/channel/{}", &self.instance, id))
+            .get(format!("{}/channel/{}", &self.instance, id.as_ref()))
+            .header("User-Agent", USER_AGENT)
             .send()
             .await?
             .text()
@@ -43,63 +48,69 @@ impl PipedClient {
         Ok(channel)
     }
 
-    pub async fn channel_continuation(
+    pub async fn channel_continuation<S: AsRef<str>>(
         &self,
-        id: String,
-        nexturl: String,
-        nextbody: String,
+        id: S,
+        nexturl: S,
+        nextbody: S,
     ) -> Result<StreamsPage, Box<dyn std::error::Error>> {
-        let mut url = Url::parse(format!("{}/nextpage/channels/{}", &self.instance, id).as_str())?;
+        let mut url = Url::parse(format!("{}/nextpage/channels/{}", &self.instance, id.as_ref()).as_str())?;
         url.query_pairs_mut()
-            .append_pair("url", nexturl.as_str())
-            .append_pair("id", nextbody.as_str());
+            .append_pair("url", nexturl.as_ref())
+            .append_pair("id", nextbody.as_ref());
 
-        let resp = &self.httpclient.get(url).send().await?.text().await?;
+        let resp = &self.httpclient.get(url)
+            .header("User-Agent", USER_AGENT)
+            .send().await?.text().await?;
 
-        let streams: StreamsPage = serde_json::from_str(resp.as_str())?;
+        let streams: StreamsPage = serde_json::from_str(resp.as_ref())?;
 
         Ok(streams)
     }
 
-    pub async fn playlist_from_id(
+    pub async fn playlist_from_id<S: AsRef<str>>(
         &self,
-        id: String,
+        id: S,
     ) -> Result<Playlist, Box<dyn std::error::Error>> {
         let resp = &self
             .httpclient
-            .get(format!("{}/playlists/{}", &self.instance, id))
+            .get(format!("{}/playlists/{}", &self.instance, id.as_ref()))
+            .header("User-Agent", USER_AGENT)
             .send()
             .await?
             .text()
             .await?;
 
-        let playlist: Playlist = serde_json::from_str(resp.as_str())?;
+        let playlist: Playlist = serde_json::from_str(resp.as_ref())?;
 
         Ok(playlist)
     }
 
-    pub async fn playlist_continuation(
+    pub async fn playlist_continuation<S: AsRef<str>>(
         &self,
-        id: String,
-        nexturl: String,
-        nextbody: String,
+        id: S,
+        nexturl: S,
+        nextbody: S,
     ) -> Result<StreamsPage, Box<dyn std::error::Error>> {
-        let mut url = Url::parse(format!("{}/nextpage/playlists/{}", &self.instance, id).as_str())?;
+        let mut url = Url::parse(format!("{}/nextpage/playlists/{}", &self.instance, id.as_ref()).as_str())?;
         url.query_pairs_mut()
-            .append_pair("url", nexturl.as_str())
-            .append_pair("id", nextbody.as_str());
+            .append_pair("url", nexturl.as_ref())
+            .append_pair("id", nextbody.as_ref());
 
-        let resp = &self.httpclient.get(url).send().await?.text().await?;
+        let resp = &self.httpclient.get(url)
+            .header("User-Agent", USER_AGENT)
+            .send().await?.text().await?;
 
         let streams: StreamsPage = serde_json::from_str(resp.as_str())?;
 
         Ok(streams)
     }
 
-    pub async fn video_from_id(&self, id: String) -> Result<VideoInfo, Box<dyn std::error::Error>> {
+    pub async fn video_from_id<S: AsRef<str>>(&self, id: S) -> Result<VideoInfo, Box<dyn std::error::Error>> {
         let resp = &self
             .httpclient
-            .get(format!("{}/streams/{}", &self.instance, id))
+            .get(format!("{}/streams/{}", &self.instance, id.as_ref()))
+            .header("User-Agent", USER_AGENT)
             .send()
             .await?
             .text()
@@ -110,48 +121,53 @@ impl PipedClient {
         Ok(video)
     }
 
-    pub async fn search_suggestions(
+    pub async fn search_suggestions<S: AsRef<str>>(
         &self,
-        q: String,
+        q: S,
     ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
         let mut url = Url::parse(format!("{}/suggestions", &self.instance).as_str())?;
-        url.query_pairs_mut().append_pair("query", q.as_str());
+        url.query_pairs_mut().append_pair("query", q.as_ref());
 
-        let resp = &self.httpclient.get(url).send().await?.text().await?;
+        let resp = &self.httpclient.get(url)
+            .header("User-Agent", USER_AGENT)
+            .send().await?.text().await?;
 
-        let suggestions: Vec<String> = serde_json::from_str(resp.as_str())?;
+        let suggestions: Vec<String> = serde_json::from_str(resp.as_ref())?;
 
         Ok(suggestions)
     }
 
-    pub async fn comments_from_id(
+    pub async fn comments_from_id<S: AsRef<str>>(
         &self,
-        id: String,
+        id: S,
     ) -> Result<CommentsInfo, Box<dyn std::error::Error>> {
         let resp = &self
             .httpclient
-            .get(format!("{}/comments/{}", &self.instance, id))
+            .get(format!("{}/comments/{}", &self.instance, id.as_ref()))
+            .header("User-Agent", USER_AGENT)
             .send()
             .await?
             .text()
             .await?;
 
-        let comments: CommentsInfo = serde_json::from_str(resp.as_str())?;
+        let comments: CommentsInfo = serde_json::from_str(resp.as_ref())?;
 
         Ok(comments)
     }
 
-    pub async fn comments_continuation(
+    pub async fn comments_continuation<S: AsRef<str>>(
         &self,
-        id: String,
-        nexturl: String,
+        id: S,
+        nexturl: S,
     ) -> Result<CommentsInfo, Box<dyn std::error::Error>> {
-        let mut url = Url::parse(format!("{}/nextpage/comments/{}", &self.instance, id).as_str())?;
-        url.query_pairs_mut().append_pair("url", nexturl.as_str());
+        let mut url = Url::parse(format!("{}/nextpage/comments/{}", &self.instance, id.as_ref()).as_str())?;
+        url.query_pairs_mut().append_pair("url", nexturl.as_ref());
 
-        let resp = &self.httpclient.get(url).send().await?.text().await?;
+        let resp = &self.httpclient.get(url)
+            .header("User-Agent", USER_AGENT)
+            .send().await?.text().await?;
 
-        let comments: CommentsInfo = serde_json::from_str(resp.as_str())?;
+        let comments: CommentsInfo = serde_json::from_str(resp.as_ref())?;
 
         Ok(comments)
     }
